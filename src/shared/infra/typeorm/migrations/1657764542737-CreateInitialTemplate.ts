f14fe418eb2f7b04a906a213e1836e89f8682b2b
Alex Paris <alex_paris28@hotmail.com>
2022-07-15 03:35:31 -0300

    fix: debug attach and generate id in migration

--- src/shared/infra/typeorm/migrations/1657764542737-CreateInitialTemplate.ts
+++ src/shared/infra/typeorm/migrations/1657764542737-CreateInitialTemplate.ts
@@ -54,6 +54,8 @@ export class CreateInitialTemplate1657764542737 implements MigrationInterface {
             name: "id",
             type: "uuid",
             isPrimary: true,
+            generationStrategy: "uuid",
+            default: "uuid_generate_v4()",
           },
           {
             name: "refresh_token",
