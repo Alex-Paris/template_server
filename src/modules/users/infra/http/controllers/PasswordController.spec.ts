66ddc7a0c115d0ef1f72dbc3b2e42f4875e65f63
Alex Paris <alex_paris28@hotmail.com>
2022-07-25 18:18:31 -0300

    chore: forgotPassword controller

---
+++ src/modules/users/infra/http/controllers/PasswordController.spec.ts
@@ -0,0 +1,80 @@
+import { verify } from "jsonwebtoken";
+import request from "supertest";
+
+import auth from "@config/auth";
+
+import { EtherealMailProvider } from "@shared/containers/providers/MailProvider/implementations/EtherealMailProvider";
+import { app } from "@shared/infra/http/app";
+import { pgDataSource } from "@shared/infra/typeorm/data-source";
+
+import { compareInHours } from "@utils/date";
+
+import { User } from "../../typeorm/entities/User";
+import { EType, UserTokens } from "../../typeorm/entities/UserTokens";
+
+let testUser: User;
+
+describe("Forgot Password Controller", () => {
+  beforeAll(async () => {
+    // Connects at database and run migrations. It must be done in each test
+    // controller file.
+    await pgDataSource.initialize();
+    await pgDataSource.runMigrations();
+
+    // Create a user at "/api/v1/user" route to let possible to test sessions
+    // controllers.
+    const { body } = await request(app).post("/api/v1/user").send({
+      name: "Name Sample",
+      email: "sample@email.com",
+      password: "samplepass",
+    });
+
+    // Put that new user information inside a var.
+    testUser = {
+      ...body,
+      password: "samplepass",
+    };
+  });
+
+  afterAll(async () => {
+    // Delete and destroy entirely data.
+    await pgDataSource.dropDatabase();
+    await pgDataSource.destroy();
+  });
+
+  it("should be able to request a forgot password to the user", async () => {
+    // Mock mail provider.
+    const sendMail = jest
+      .spyOn(EtherealMailProvider.prototype, "sendMail")
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      .mockImplementationOnce(async () => {});
+
+    // Request recovery password mail.
+    const response = await request(app).post("/api/v1/password/forgot").send({
+      email: testUser.email,
+    });
+
+    // Get the refresh token saved at database for expecting test analisys.
+    const userToken = (await pgDataSource
+      .createQueryBuilder<UserTokens>(UserTokens, "users_tokens")
+      .where("users_tokens.user_id = :id", { id: testUser.id })
+      .getOne()) as UserTokens;
+
+    const { forgotSecret, forgotExpiresIn } = auth.jwt;
+    const { type, created_at, expires_at, refresh_token } = userToken;
+
+    expect(response.status).toBe(204);
+    expect(sendMail).toHaveBeenCalled();
+    expect(type).toBe(EType.forgot_password);
+    expect(verify(refresh_token, forgotSecret));
+    expect(compareInHours(created_at, expires_at)).toBe(forgotExpiresIn);
+  });
+
+  it("should not be able to request a forgot password with an unfound email", async () => {
+    const response = await request(app).post("/api/v1/password/forgot").send({
+      email: "invalid@email.com",
+    });
+
+    expect(response.status).toBe(404);
+  });
+});
