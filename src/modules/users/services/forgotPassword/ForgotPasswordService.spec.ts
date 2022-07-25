42a9f271997c0935e92131354cf29e2b6a39f661
Alex Paris <alex_paris28@hotmail.com>
2022-07-25 18:19:20 -0300

    fix: forgotPassword spec service expect improve

--- src/modules/users/services/forgotPassword/ForgotPasswordService.spec.ts
+++ src/modules/users/services/forgotPassword/ForgotPasswordService.spec.ts
@@ -1,9 +1,18 @@
+import { verify } from "jsonwebtoken";
+
 import { User } from "@modules/users/infra/typeorm/entities/User";
-import { UserTokens } from "@modules/users/infra/typeorm/entities/UserTokens";
+import {
+  EType,
+  UserTokens,
+} from "@modules/users/infra/typeorm/entities/UserTokens";
 import { MockUsersRepository } from "@modules/users/repositories/mocks/MockUsersRepository";
 import { MockUsersTokensRepository } from "@modules/users/repositories/mocks/MockUsersTokensRepository";
 
+import auth from "@config/auth";
+
 import { MockMailProvider } from "@shared/containers/providers/MailProvider/mocks/MockMailProvider";
+
+import { compareInHours } from "@utils/date";
 
 import { ForgotPasswordError } from "./ForgotPasswordError";
 import { ForgotPasswordService } from "./ForgotPasswordService";
@@ -41,14 +50,36 @@ describe("Forgot Password Service", () => {
   });
 
   it("should be able to recover the password using the email", async () => {
+    // Spy mocked providers.
     const sendMail = jest.spyOn(mockMailProvider, "sendMail");
+    const createToken = jest.spyOn(mockUsersTokensRepository, "create");
 
+    // Request recovery password mail.
     await forgotPasswordService.execute({
       email: testUser.email,
       remote_address: "127.0.0.1",
     });
 
+    // Gets token returned in mock or create a new one if its null.
+    const forgotToken = (await createToken.mock.results[0].value) as UserTokens;
+
+    // Get the refresh token saved at mock provider for expecting test analisys.
+    const { forgotSecret, forgotExpiresIn } = auth.jwt;
+    const {
+      type,
+      created_at,
+      created_by_ip,
+      expires_at,
+      refresh_token,
+      user_id,
+    } = forgotToken;
+
     expect(sendMail).toHaveBeenCalled();
+    expect(type).toBe(EType.forgot_password);
+    expect(verify(refresh_token, forgotSecret));
+    expect(user_id).toBe(testUser.id);
+    expect(created_by_ip).toBe("127.0.0.1");
+    expect(compareInHours(created_at, expires_at)).toBe(forgotExpiresIn);
   });
 
   it("should not be able to recover with an unfound email", async () => {
@@ -58,21 +89,5 @@ describe("Forgot Password Service", () => {
         remote_address: "127.0.0.1",
       })
     ).rejects.toBeInstanceOf(ForgotPasswordError.UserNotFound);
-  });
-
-  it("should generate a forgot password token", async () => {
-    const createToken = jest.spyOn(mockUsersTokensRepository, "create");
-
-    await forgotPasswordService.execute({
-      email: testUser.email,
-      remote_address: "127.0.0.1",
-    });
-
-    // Gets token returned in mock or create a new one if its null.
-    const forgotToken = (await createToken.mock.results[0].value) as UserTokens;
-
-    expect(createToken).toHaveBeenCalled();
-    expect(forgotToken.user_id).toBe(testUser.id);
-    expect(forgotToken.created_by_ip).toBe("127.0.0.1");
   });
 });
