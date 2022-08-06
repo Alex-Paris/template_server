d63dbb94e7ee6b661d6f2c9d086e5e45b4d19fa3
Alex Paris <alex_paris28@hotmail.com>
2022-08-05 21:50:31 -0300

    chore: update avatar controller test

--- src/modules/users/infra/http/controllers/UserController.spec.ts
+++ src/modules/users/infra/http/controllers/UserController.spec.ts
@@ -2,15 +2,20 @@ import request from "supertest";
 
 import { ICreateUserDTO } from "@modules/users/dtos/ICreateUserDTO";
 import { CreateUserError } from "@modules/users/services/user/create/CreateUserError";
+import { UpdateAvatarUserError } from "@modules/users/services/user/updateAvatar/UpdateAvatarUserError";
 
+import { DiskStorageProvider } from "@shared/containers/providers/StorageProvider/implementations/DiskStorageProvider";
 import { app } from "@shared/infra/http/app";
 import { pgDataSource } from "@shared/infra/typeorm/data-source";
 
 import { User } from "../../typeorm/entities/User";
+import { UsersRepository } from "../../typeorm/repositories/UsersRepository";
 
+let testUser: User;
+let testToken: string;
 let newUser: ICreateUserDTO;
 
-describe("User Controller", () => {
+describe("Create User Controller", () => {
   beforeAll(async () => {
     // Connects at database and run migrations. It must be done in each test
     // controller file.
@@ -54,5 +59,125 @@ describe("User Controller", () => {
 
     expect(response.status).toBe(statusCode);
     expect(response.body.message).toBe(message);
+  });
+});
+
+describe("Update Avatar User Controller", () => {
+  beforeAll(async () => {
+    // Connects at database and run migrations. It must be done in each test
+    // controller file.
+    await pgDataSource.initialize();
+    await pgDataSource.runMigrations();
+
+    // Create a user at "/api/v1/user" route to let possible to test.
+    const { body: newUser } = await request(app).post("/api/v1/user").send({
+      name: "Name Sample",
+      email: "sample@email.com",
+      password: "samplepass",
+    });
+
+    // Put that new user information inside a var.
+    testUser = {
+      ...newUser,
+      password: "samplepass",
+    };
+
+    // Authenticate user.
+    const { body: authToken } = await request(app)
+      .post("/api/v1/session/authenticate")
+      .send({
+        email: testUser.email,
+        password: testUser.password,
+      });
+
+    // Put token inside a var.
+    testToken = authToken.token;
+  });
+
+  afterAll(async () => {
+    // Delete and destroy entirely data.
+    await pgDataSource.dropDatabase();
+    await pgDataSource.destroy();
+  });
+
+  it("should be able to update user avatar", async () => {
+    // Mock disk storage to fake save file function.
+    jest
+      .spyOn(DiskStorageProvider.prototype, "saveFile")
+      .mockImplementation(async () => {
+        return "avatar.jpg";
+      });
+
+    // Update avatar user with mocked image.
+    const response = await request(app)
+      .patch("/api/v1/user/avatar")
+      .set({
+        Authorization: `Bearer ${testToken}`,
+      });
+
+    // Get the updated user for expecting test analisys.
+    const user = (await pgDataSource
+      .createQueryBuilder<User>(User, "users")
+      .where("users.id = :id", { id: testUser.id })
+      .getOne()) as User;
+
+    expect(response.status).toBe(200);
+    expect(response.body.avatar).toBe("avatar.jpg");
+    expect(response.body.avatar).toBe(user.avatar);
+  });
+
+  it("should not be able to update avatar from non existing user", async () => {
+    // Mock user repository to fake authenticate middleware to not fount user.
+    jest
+      .spyOn(UsersRepository.prototype, "findById")
+      .mockImplementationOnce(async () => {
+        return null;
+      });
+
+    // Update avatar route with a valid token.
+    const response = await request(app)
+      .patch("/api/v1/user/avatar")
+      .set({
+        Authorization: `Bearer ${testToken}`,
+      });
+
+    // Get code and message error expected.
+    const { statusCode, message } =
+      new UpdateAvatarUserError.NotAuthenticatedUser();
+
+    expect(response.status).toBe(statusCode);
+    expect(response.body.message).toBe(message);
+  });
+
+  it("should delete old avatar when updating with a new one", async () => {
+    // Mock disk storage to fake save file function.
+    jest
+      .spyOn(DiskStorageProvider.prototype, "saveFile")
+      .mockImplementation(async () => {
+        return "avatar2.jpg";
+      });
+
+    // Mock disk storage to fake save file function.
+    const spyDelete = jest
+      .spyOn(DiskStorageProvider.prototype, "deleteFile")
+      .mockImplementation();
+
+    // Update avatar router by the second time. Old image must be deleted.
+    const response = await request(app)
+      .patch("/api/v1/user/avatar")
+      .set({
+        Authorization: `Bearer ${testToken}`,
+      });
+
+    // Get the updated user for expecting test analisys.
+    const user = (await pgDataSource
+      .createQueryBuilder<User>(User, "users")
+      .where("users.id = :id", { id: testUser.id })
+      .getOne()) as User;
+
+    expect(response.status).toBe(200);
+    expect(spyDelete).toBeCalledTimes(1);
+    expect(response.body.avatar).toBe("avatar2.jpg");
+    expect(response.body.avatar).toBe(user.avatar);
   });
 });
